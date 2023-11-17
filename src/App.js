b7661c3df6d9df19bc016924ac851c219f08678a
mpatin <mitchell.patin@gmail.com>
2023-11-16 18:16:11 -0800

    added tableflow importer

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+import { TableFlowImporter } from '@tableflow/react';
+
+export default function App() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClose = () => { setIsOpen(false); };
+
+    return (
+      <>
+        <button className="buttonStyle" onClick={() => setIsOpen(true)}>Open TableFlow Importer</button>
+  
+        <TableFlowImporter
+          importerId={"b023c8c8-101d-40c7-b279-bd4a131da516"} // Use your importer ID
+          modalIsOpen={isOpen}
+          modalOnCloseTriggered={() => setIsOpen(false)}
+          primaryColor="#246A24"
+          metadata={{"user_id": 1234}}
+          onComplete={(data) => console.log(data)}
+        />
+      </>
+    );
+
+}
